44f37baeddeeb02df06026da09c66bd4247756cd
Roman Edouard <123408909+romanedouard@users.noreply.github.com>
2024-05-03 02:13:39 -0400

    add JavaScript logic to make the calculator work

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
 const calcInput = document.querySelector('.calc-input');
+const allButtons = document.querySelectorAll('.button');
+
+allButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const value = e.target.dataset.value;
+
+        if (value === '=') {
+            if (calcInput.innerText.includes('/')) {
+                const splitStuff = calcInput.innerText.split('/');
+                operate('/', splitStuff[0], splitStuff[1]);
+            }
+        }
+
+        if (value === '=') {
+            if (calcInput.innerText.includes('-')) {
+                const splitStuff = calcInput.innerText.split('-');
+                operate('-', splitStuff[0], splitStuff[1]);
+            }
+        }
+
+        if (value === '=') {
+            if (calcInput.innerText.includes('+')) {
+                const splitStuff = calcInput.innerText.split('+');
+                operate('+', splitStuff[0], splitStuff[1]);
+            }
+        }
+
+        if (value === '=') {
+            if (calcInput.innerText.includes('*')) {
+                const splitStuff = calcInput.innerText.split('*');
+                operate('*', splitStuff[0], splitStuff[1]);
+            }
+        }
+
+        calcInput.innerText += value;
+
+        if (value === 'clear') {
+            calcInput.innerText = '';
+        }
+    })
+})
+
+const operate = (operator, value1, value2) => {
+    const num1 = Number(value1);
+    const num2 = Number(value2);
+
+    if (operator === '/') {
+        calcInput.innerText = num1 / num2;
+    }
+
+    if (operator === '-') {
+        calcInput.innerText = num1 - num2;
+    }
+
+    if (operator === '+') {
+        calcInput.innerText = num1 + num2;
+    }
+
+    if (operator === '*') {
+        calcInput.innerText = num1 * num2;
+    }
+}
